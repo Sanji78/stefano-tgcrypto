5d5e59f9082267a19434bc0c64c9573018c89ab2
Sanji78 <stefano.giugliano78@gmail.com>
2026-03-06 10:29:31 +0100

    Update build for Python 3.13 and 3.14

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="stefano-tgcrypto",
-    version="1.4.1",
+    version="1.4.2",
     description="Fast and Portable Cryptography Extension Library for Pyrogram",
     long_description=readme,
     long_description_content_type="text/markdown",
